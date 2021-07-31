13b424dc6f0061794f9e0be1a6c630a588cf45a0
gennova <serendipity.10they@gmail.com>
2021-07-31 13:36:06 +0700

    second

--- admin/admin/adminaduan.php
+++ admin/admin/adminaduan.php
@@ -43,6 +43,8 @@ $result = mysqli_query($con,$sql_query);
     white-space: nowrap;
 }
   </style>
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css" />
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css" />
 </head>
 
 <body class="">
@@ -185,7 +187,7 @@ $result = mysqli_query($con,$sql_query);
                 <div class="card-body">
                   <div class="tab-content">
                     <div class="tab-pane active" id="profile">             
-                      <table class="table">
+                      <table id="example" class="display">
                       <thead class=" text-primary">
                         <th>
                           No
@@ -352,13 +354,36 @@ $result = mysqli_query($con,$sql_query);
   <script src="../assets/js/material-dashboard.js?v=2.1.2" type="text/javascript"></script>
   <!-- Material Dashboard DEMO methods, don't include it in your project! -->
   <script src="../assets/demo/demo.js"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
+<script>
+  $(document).ready(function() {
+    $('#example').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5'
+        ]
+    } );
+} );
+</script>
   <script>
     $(document).ready(function() {
       // Javascript method's body can be found in assets/js/demos.js
+      //cek
       md.initDashboardPageCharts();
 
     });
   </script>
+
+
 </body>
 
 </html>
